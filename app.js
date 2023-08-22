8a9683da66d2fcc5029f96590e5fad9160d6e11c
Shubham Goswami <shubham24976@gmail.com>
2023-08-22 16:50:22 +0530

    Security HTTP Headers with helmet

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
 const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 const moviesRouter = require('./Routes/moviesRoutes');
 const authRouter = require('./Routes/authRouter');
 const CustomError = require('./Utils/CustomError');
@@ -9,23 +10,34 @@ const globalErrorHandler = require('./Controllers/errorController');
 
 const app = express();
 
-app.use(express.json());
-
 // Global Middlewares
+
+// Set Security HTTP Headers
+app.use(helmet());
+
+// Development Logging
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
 
+// Limit requests from same IP (Rate - Limiting)
 const limiter = rateLimit({
-    max: 3,
+    max: 100,
     windowMs: 60 * 60 * 1000,
     message: 'Too many requests from this IP. Please try after an hour! :)'
 });
 app.use('/api', limiter);
 
+// Body-Parser, reading data from body into req.body
+app.use(express.json({ limit: '10kb' })); // limit the amount of data to req.body (Security measures)
+
+// Serving Static Files
 app.use(express.static('./public'));
-app.use((req, res, next) => { // Middleware
+
+// Test Middleware
+app.use((req, res, next) => { 
     req.requestedAt = new Date().toISOString();
+    //console.log(req.headers);
     next();
 })
 

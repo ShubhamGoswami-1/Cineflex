9639058b78d39767f734b526dffd59b3f4a6774d
Shubham Goswami <shubham24976@gmail.com>
2023-08-22 16:31:40 +0530

    Rate limiting & set jwt as httpsOnly Cookie

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require('express');
 const fs = require('fs');
 const morgan = require('morgan');
+const rateLimit = require('express-rate-limit');
 const moviesRouter = require('./Routes/moviesRoutes');
 const authRouter = require('./Routes/authRouter');
 const CustomError = require('./Utils/CustomError');
@@ -10,9 +11,17 @@ const app = express();
 
 app.use(express.json());
 
+// Global Middlewares
 if(process.env.NODE_ENV === 'development'){
     app.use(morgan('dev'));
 }
+
+const limiter = rateLimit({
+    max: 3,
+    windowMs: 60 * 60 * 1000,
+    message: 'Too many requests from this IP. Please try after an hour! :)'
+});
+app.use('/api', limiter);
 
 app.use(express.static('./public'));
 app.use((req, res, next) => { // Middleware
